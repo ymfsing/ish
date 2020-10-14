edda200d7f48f22e86510693135fd2158ae4c675
ymfsing <ymfsing@outlook.com>
2020-10-14 14:00:47 +0800

    update .emacs.d org, delete org-roam-server

--- .emacs.d/lisp/lang/init-org.el
+++ .emacs.d/lisp/lang/init-org.el
@@ -3,7 +3,7 @@
 
 (use-package org
   :config
-  (setq org-directory "~/Documents/org")
+  (setq org-directory "~/org")
   (setq org-default-notes-file (concat org-directory "/inbox.org")))
 
 
@@ -11,25 +11,9 @@
   :hook
   (after-init . org-roam-mode)
   :custom
-  (org-roam-directory "~/Documents/org/roam")
+  (org-roam-directory "~/org/roam")
   :config
   (setq org-roam-db-gc-threhold most-positive-fixnum))
-
-
-(use-package org-roam-server
-  :config
-  (setq org-roam-server-host "127.0.0.1"
-        org-roam-server-port 8080
-        org-roam-server-authenticate nil
-        org-roam-server-export-inline-images t
-        org-roam-server-serve-files nil
-        org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
-        org-roam-server-network-poll t
-        org-roam-server-network-arrows nil
-        org-roam-server-network-label-truncate t
-        org-roam-server-network-label-truncate-length 60
-        org-roam-server-network-label-wrap-length 20)
-  (require 'org-roam-protocol))
 
 
 (provide 'init-org)
